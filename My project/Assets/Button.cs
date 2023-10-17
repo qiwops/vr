9262d69cc067bdecccb510894dfce73a272fced1
qiwops <dariyavasileva00@mail.ru>
2023-10-17 21:36:34 +0300

    commit

---
+++ My project/Assets/Button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject Sphere;
+    public GameObject Cube;
+    
+    public void onClick()
+    {
+        if (Sphere.activeSelf)
+        {
+            Sphere.SetActive(false);
+            Cube.SetActive(false);
+        }
+        else
+        {
+            Sphere.SetActive(true);
+            Cube.SetActive(true);
+        }
+    }
+}
